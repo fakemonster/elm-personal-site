1d5c77b540331aba81413cf10983a49940d96758
Joe Thel <jgraythel@gmail.com>
2021-01-18 13:55:47 -0800

    🔒 redirect to https when supported

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const elmServe = require('./elmServe')
+const httpsUpgrade = require('./httpsUpgrade')
 
 process.env.PORT = process.env.PORT || '3000'
 const app = express()
 
+const info = console.info
+
+if (process.env.DEPLOYED === 'true') {
+  info('Running in deployment mode...')
+  info(`-- ${httpsUpgrade.description}`)
+  app.set('trust proxy', true)
+  app
+    .use(httpsUpgrade)
+}
+
 elmServe(app)
-  .listen(process.env.PORT, () => console.info('lets do it'))
+  .listen(process.env.PORT, () => info('lets do it'))
